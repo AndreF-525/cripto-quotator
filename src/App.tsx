c8f57e3a98e1ee09292df55939b2095096c5be71
André Montaño <andrefernando2000@gmail.com>
2025-08-22 17:05:20 -0400

    axios, zustand, store

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react"
 import SearchForm from "./components/SearchForm"
+import { useCryptoStore } from "./store"
 
 function App() {
+
+  const fetchCryptos = useCryptoStore((state) => state.fetchCryptos)
+
+  useEffect(() => {
+    fetchCryptos()
+  }, [])
 
   return (
     <>
