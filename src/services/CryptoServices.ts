91af2902bbc2fe901699d64f2d311a9e163cf330
André Montaño <andrefernando2000@gmail.com>
2025-09-22 15:47:01 -0400

    schema de la respuesta

--- src/services/CryptoServices.ts
+++ src/services/CryptoServices.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { CryptoCurrencySchema } from "../schema/crypto-schema"
+import { CryptoCurrencySchema, CryptoPriceSchema } from "../schema/crypto-schema"
 import type { Pair } from "../types"
 
 export async function getCryptos() {
@@ -16,5 +16,9 @@ export async function fetchCurrentCryptoPrice(pair : Pair) {
   const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${pair.cryptocurrency}&tsyms=${pair.currency}`
 
   const { data: {DISPLAY} } = await axios(url)
-  console.log(DISPLAY[pair.cryptocurrency][pair.currency])
+  const result = CryptoPriceSchema.safeParse(DISPLAY[pair.cryptocurrency][pair.currency])
+  
+  if (result.success) {
+    return result.data
+  }
 }
